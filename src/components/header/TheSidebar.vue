baf1308d80f590c427fc4657b0a3a730be22d1a1
Haroon Nafees <haroon.nafees@gmail.com>
2024-05-13 22:43:02 +0500

    sidebar icon added

--- src/components/header/TheSidebar.vue
+++ src/components/header/TheSidebar.vue
@@ -3,24 +3,23 @@
     <span class="material-symbols-outlined close-icon"  @click="menuOpen=!menuOpen">close</span>
     <div class="logo"><b>CRM.io</b></div>
     <ul class="menu">
-      <li><a href="" class="active"><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
+      <li><a href="" class="active"><span class="material-symbols-outlined">dashboard</span>Dasboard</a></li>
       <li>
         <span class="submenu">Managment</span>
         <ul>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
+          <li><a href=""><span class="material-symbols-outlined">folder_open</span>Document</a></li>
+          <li><a href=""><span class="material-symbols-outlined">contact_support</span>Contact</a></li>
+          <li><a href=""><span class="material-symbols-outlined">psychology</span>Prospect</a></li>
+          <li><a href=""><span class="material-symbols-outlined">rebase</span>Workflow</a></li>
         </ul>
       </li>
 
       <li>
-        <span class="submenu">Managment</span>
+        <span class="submenu">Connection</span>
         <ul>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
-          <li><a href=""><span class="material-symbols-outlined">dashboard</span>Documents</a></li>
+          <li><a href=""><span class="material-symbols-outlined">headset_mic</span>Chat Integration</a></li>
+          <li><a href=""><span class="material-symbols-outlined">linked_services</span>Marketing Automation</a></li>
+          <li><a href=""><span class="material-symbols-outlined">email</span>Email Integration</a></li>
         </ul>
       </li>
 
